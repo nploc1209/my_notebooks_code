586a00ca2e677e29d284750d23c6ad710d95208c
Nguyễn Phúc Lộc <148461258+nploc1209@users.noreply.github.com>
2024-02-24 17:06:50 +0000

    First initial

---
+++ main_template.cpp
@@ -0,0 +1,27 @@
+/* 
+ * Thuat toan: ten_thuat_toan
+ * Do phuc tap: O(do_phuc_tap)
+ * Y tuong: y_tuong
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    time_t start, end;
+    time(&start);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    freopen(".INP", "r", stdin);
+    freopen(".OUT", "w", stdout);
+    
+    //ma_nguon
+
+    time(&end); 
+    cout << "Run time: " << double(end - start) << "s\n";
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,27 @@
+/* 
+ * Thuat toan: Test
+ * Do phuc tap: O(1)
+ * Y tuong: In "Hello, World!"
+ * 
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    time_t start, end;
+    time(&start);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    freopen(".INP", "r", stdin);
+    freopen(".OUT", "w", stdout);
+
+    cout << "Hello, World!\n";
+
+    time(&end); 
+    cout << "Run time: " << double(end - start) << "s\n";
+    return 0;
+}   
